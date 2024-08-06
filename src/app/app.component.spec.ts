e8c8449991bdcea1ec342517b53b30b7cdf931da
anujdonvir <anujdonvir@gmail.com>
2024-08-05 23:26:25 -0400

    code cleanup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,6 @@ describe('AppComponent', () => {
   it(`should have as title 'emp-time-tracker-1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('emp-time-tracker-1');
   });
 
   it('should render title', () => {
